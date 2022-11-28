44fb8d9615026811cac1f3c0501e53b42e57d75b
Abhinav-676 <harshikeshabhinav670@gmail.com>
2022-11-28 06:29:22 +0530

    improved code

--- script/index.js
+++ script/index.js
@@ -3,33 +3,28 @@ let theme = 0;
 
 
 const changeTheme = () => {
+    
+    const ThemeButton = document.getElementsByClassName('theme-button')
+
     if(theme == 0)
     {
-        let ThemeButton = document.getElementsByClassName('theme-button')
-
-        ThemeButton[0].classList.remove('dark-theme-shadow')
-        ThemeButton[1].classList.remove('dark-theme-shadow')
-
         ThemeButton[0].firstElementChild.src = "img/Untitledlight.svg";
-        ThemeButton[0].classList.add('light-theme-shadow')
+        ThemeButton[0].classList.replace('dark-theme-shadow', 'light-theme-shadow')
 
         ThemeButton[1].firstElementChild.src = "img/Untitledlight.svg";
-        ThemeButton[1].classList.add('light-theme-shadow')
+        ThemeButton[1].classList.replace('dark-theme-shadow','light-theme-shadow')
         
         theme = 1
-    } else {
-        let ThemeButton = document.getElementsByClassName('theme-button')
 
-        ThemeButton[0].classList.remove('light-theme-shadow')
-        ThemeButton[1].classList.remove('light-theme-shadow')
+    } else {
 
         ThemeButton[0].firstElementChild.src = "img/Untitleddark.svg";
-        ThemeButton[0].classList.add('dark-theme-shadow')
+        ThemeButton[0].classList.replace('light-theme-shadow', 'dark-theme-shadow')
 
         ThemeButton[1].firstElementChild.src = "img/Untitleddark.svg";
-        ThemeButton[1].classList.add('dark-theme-shadow')
-        
+        ThemeButton[1].classList.replace('light-theme-shadow', 'dark-theme-shadow')
+
         theme = 0
     }
-
 }
+
