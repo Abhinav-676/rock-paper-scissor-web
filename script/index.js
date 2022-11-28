2afd4a4d692de4f7eef10d667422d33dea0b14dd
Abhinav-676 <harshikeshabhinav670@gmail.com>
2022-11-28 18:45:39 +0530

    Improved code

--- script/index.js
+++ script/index.js
@@ -1,43 +1,20 @@
 
-let theme = 0
-const root = document.querySelector(':root')
-let themeImage = document.getElementsByClassName('theme-icon')
-
-
-function renderLightTheme() {
-    themeImage[0].src = "img/Untitleddark.svg"
-    themeImage[1].src = "img/Untitleddark.svg"
-    root.style.setProperty('background-color','#f5f5f5')
-    root.style.setProperty('--nav-bg','#EF8354')
-    root.style.setProperty('--title_color','#f5f5f5')
-    root.style.setProperty('--themeButton-shadow-idle','0px 0px 4px 2px rgba(0, 0, 0, 0.75)')
-    root.style.setProperty('--themeButton-shadow-active','0px 0px 3px 1px rgba(0, 0, 0, 0.75)')
-    root.style.setProperty('--footer-color','#4F5D75')
-}
-
-function renderDarkTheme() {
-    themeImage[0].src = "img/Untitledlight.svg"
-    themeImage[1].src = "img/Untitledlight.svg"
-    root.style.setProperty('background-color','#6C757D')
-    root.style.setProperty('--nav-bg','#343A40')
-    root.style.setProperty('--title_color','#f5f5f5')
-    root.style.setProperty('--themeButton-shadow-idle','0px 0px 4px 2px #fee127be')
-    root.style.setProperty('--themeButton-shadow-active','0px 0px 3px 1px #fee127be')
-    root.style.setProperty('--footer-color','#343A40')
-}
-
-renderLightTheme()
-
 const changeTheme = () => {
+    const mode = document.querySelector('html').getAttribute('data-theme')
+    const themeImage = document.getElementsByClassName('theme-icon')
 
-    if (theme == 0)
+    if (mode == "light")
     {
-        renderDarkTheme()
-        theme = 1
+        themeImage[0].src = "img/Untitledlight.svg"
+        themeImage[1].src = "img/Untitledlight.svg"
+    
+        document.querySelector('html').setAttribute('data-theme', 'dark')
 
     }else {
-        renderLightTheme()
-        theme = 0
+        themeImage[0].src = "img/Untitleddark.svg"
+        themeImage[1].src = "img/Untitleddark.svg"
+    
+        document.querySelector('html').setAttribute('data-theme', 'light')
     }
 }
 
